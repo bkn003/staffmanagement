06399a8e3f7aa7a32641b6dd6b07f43851fb968a
bkn003 <bkndhn03@proton.me>
2025-07-15 10:32:09 +0530

    Updated salaryCalculations.ts

--- src/utils/salaryCalculations.ts
+++ src/utils/salaryCalculations.ts
@@ -187,9 +187,9 @@ export const calculateSalary = (
   advances: AdvanceDeduction | null,
   allAdvances: AdvanceDeduction[],
   attendance: Attendance[],
-  attendance: Attendance[],
   currentMonth: number,
   currentYear: number
+) => {
   const { totalPresentDays, sundayAbsents, daysInMonth, presentDays, halfDays, leaveDays } = attendanceMetrics;
   
   let basicEarned: number;
